073f4cf61187b62a275f761745096f1b5764c673
한승규 <trust1204@sampyo.co.kr>
2026-05-20 11:06:53 +0900

    first commit

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,23 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function initSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('haruteum backend')
+    .setDescription('haruteum backend API documentation')
+    .setVersion('0.0.1')
+    .addServer('/v1')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'Bearer',
+        in: 'header',
+      },
+      'jwt',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+}
