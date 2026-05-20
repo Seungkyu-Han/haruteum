79427c67dee683b960a93b27b7860edb2f06c989
한승규 <trust1204@sampyo.co.kr>
2026-05-20 13:30:58 +0900

    feat: set google chat error reporter

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ErrorReporterModule } from '@seungkyu/error-reporter';
 
-@Module({})
+@Module({
+  imports: [
+    ErrorReporterModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'slack',
+        webhookUrl: configService.getOrThrow('WEBHOOK_URL'),
+        serverName: configService.getOrThrow('SERVER_NAME'),
+      }),
+    }),
+  ],
+})
 export class ConfigModule {}
