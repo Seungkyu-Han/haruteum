57c549d21af5496c84fd3a652bade8b3c2b7a8ca
한승규 <trust1204@sampyo.co.kr>
2026-05-22 09:46:19 +0900

    feat: add TypeORM support with PostgreSQL configuration
    - Updated package.json to include @nestjs/typeorm and pg dependencies.
    - Enhanced package-lock.json with new dependencies and updated versions.
    - Implemented TypeOrmModule in config.module.ts for PostgreSQL connection using ConfigService.

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -3,6 +3,7 @@ import {
   ConfigService,
   ConfigModule as NestConfigModule,
 } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ErrorReporterModule } from '@seungkyu/error-reporter';
 
 @Module({
@@ -17,6 +18,20 @@ import { ErrorReporterModule } from '@seungkyu/error-reporter';
         type: 'slack',
         webhookUrl: configService.getOrThrow('WEBHOOK_URL'),
         serverName: configService.getOrThrow('SERVER_NAME'),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [NestConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow('DB_HOST'),
+        port: configService.getOrThrow('DB_PORT'),
+        username: configService.getOrThrow('DB_USERNAME'),
+        password: configService.getOrThrow('DB_PASSWORD'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        database: configService.getOrThrow('DB_NAME'),
+        synchronize: true,
       }),
     }),
   ],
