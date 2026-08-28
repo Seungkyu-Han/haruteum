daa2bdd5bb738209905d0ab31e10faf8aacb388a
Seungkyu-Han <trust1204@gmail.com>
2026-08-28 22:50:34 +0900

    Add DTOs for memory list and memory element responses in memory API to standardize response structure

---
+++ src/memory/memory-api/dto/response/memory-element.response.dto.ts
@@ -0,0 +1,33 @@
+import { ApiProperty, ApiSchema } from '@nestjs/swagger';
+
+@ApiSchema({ name: '추억 요소 응답 API DTO' })
+export class MemoryElementResponseDto {
+  @ApiProperty({
+    description: '추억의 아이디',
+    example: '1Bdfdh-asdfasf',
+  })
+  memoryId: string;
+
+  @ApiProperty({
+    description: '추억이 생성된 날짜',
+    example: '2023-10-01T12:00:00Z',
+  })
+  createdAt: Date;
+
+  @ApiProperty({
+    description: '추억에 대한 감정',
+    example: ['행복해', '즐거워'],
+  })
+  emotions: string[];
+
+  @ApiProperty({
+    description: '행복 점수',
+    example: 85,
+  })
+  happyScore: number;
+
+  @ApiProperty({
+    description: '사진 주소',
+  })
+  images: string[];
+}
---
+++ src/memory/memory-api/dto/response/memory-list.response.dto.ts
@@ -0,0 +1,16 @@
+import { MemoryElementResponseDto } from './memory-element.response.dto';
+import { ApiProperty, ApiSchema } from '@nestjs/swagger';
+
+@ApiSchema({ name: '추억 리스트 응답 API DTO' })
+export class MemoryListResponseDto {
+  @ApiProperty({
+    description: '추억 요소들',
+  })
+  memories: MemoryElementResponseDto[];
+
+  @ApiProperty({
+    description: '다음 페이지 존재 여부',
+    example: true,
+  })
+  hasNext: boolean;
+}
