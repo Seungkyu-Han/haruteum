75c76181e36141ab872405733b89b59dbafca68f
Seungkyu-Han <trust1204@gmail.com>
2026-08-28 10:12:26 +0900

    Remove support for MEMORY_IMAGE_STORAGE from MemoryInfraModule

--- src/memory/memory-infra/memory-infra.module.ts
+++ src/memory/memory-infra/memory-infra.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import {
   IMAGE_MOOD_AGENT,
-  MEMORY_IMAGE_STORAGE,
   MEMORY_REPOSITORY,
 } from '../memory-core/memory.token';
 import { OpenAIImageMoodAgent } from './agents/openai_image_mood_agent';
-import { MinioMemoryImageStorage } from './storages/minio-memory-image.storage';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MockImageMoodAgent } from './agents/mock_image_mood_agent';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -46,34 +44,7 @@ import { MemoryRepositoryPg } from './repositories/memory.repository.pg';
         }
       },
     },
-    {
-      inject: [ConfigService],
-      provide: MEMORY_IMAGE_STORAGE,
-      useFactory: (configService: ConfigService) => {
-        const memoryImageStorageType = configService.get<string>(
-          'MEMORY_IMAGE_STORAGE_TYPE',
-        );
-        const localIp = configService.get<string>('LOCAL_IP') || '';
-        const s3AccessKeyId =
-          configService.get<string>('S3_ACCESS_KEY_ID') || '';
-        const s3SecretAccessKey =
-          configService.get<string>('S3_SECRET_ACCESS_KEY') || '';
-
-        switch (memoryImageStorageType) {
-          case 'minio':
-            return new MinioMemoryImageStorage(
-              localIp,
-              s3AccessKeyId,
-              s3SecretAccessKey,
-            );
-          default:
-            throw new Error(
-              `Unsupported MEMORY_IMAGE_STORAGE_TYPE: ${memoryImageStorageType}`,
-            );
-        }
-      },
-    },
   ],
-  exports: [IMAGE_MOOD_AGENT, MEMORY_IMAGE_STORAGE, MEMORY_REPOSITORY],
+  exports: [IMAGE_MOOD_AGENT, MEMORY_REPOSITORY],
 })
 export class MemoryInfraModule {}
