57e14f109cf5169caf0970931e2f5f9e92940f42
Seungkyu-Han <trust1204@gmail.com>
2026-09-02 14:15:50 +0900

    feat: integrate global validation pipe with whitelist and transformation rules

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { initSwagger } from './config/swagger.config';
 import { initVersioning } from './config/versioning.config';
 import { initCorsConfig } from './config/cors.config';
 import { initErrorMapper } from './config/error-mapper.config';
+import { initValidationPipe } from './config/validation-pipe.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
   initVersioning(app);
   initCorsConfig(app);
   initErrorMapper(app);
+  initValidationPipe(app);
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 void bootstrap();
